d5bc0b411b351ee7a94d5cbaeb17e671cbc418ba
Serge Cage <sergeycage@rocketmail.com>
2024-10-07 22:12:06 +0300

    feat:solve the first task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const dir = require('path');
+
 const create = async () => {
-    // Write your code here 
+    fs.mkdir(path.join(__dirname, "files"));
+
+
+    fs.writeFile(
+        path.join(__dirname, "files", 'fresh.txt'),
+        "I am fresh and young",
+     () => {
+        if(fs.existsSync) {
+            throw new Error("FS operation failed");
+        }
+     },)
+
 };
 
 await create();
